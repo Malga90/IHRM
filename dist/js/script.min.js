35a49270aae8435ca7d1808897684d04277b0dca
Malga90 <savemeebarry@gmail.com>
2018-11-15 22:15:29 +0100

    sticky menu in vanilla js

--- dist/js/script.min.js
+++ dist/js/script.min.js
@@ -1,1 +1,26 @@
-console.log('Gruszka to dzidzia, hihih');
+console.log('Gruszka jest malutka');
+
+
+
+// Sticky navigation
+
+const nav = document.querySelector('.navigation');
+const navTop = nav.offsetTop;
+
+function stickyNavigation() {
+    console.log('navTop = ' + navTop);
+    console.log('scrollY = ' + window.scrollY);
+
+    if  (window.scrollY >= navTop) {
+        document.body.style.paddingTop = nav.OffsetHeight + 'px';
+        document.body.classList.add('fixed-nav');
+    } else {
+        document.body.style.paddingTop = navOffsetHeight + 'px';
+        document.body.classList.remove('fixed-nav');
+    }
+}
+
+window.addEventListener('scroll', stickyNavigation);
+
+
+
