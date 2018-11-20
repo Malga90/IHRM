c5ef784fa47f30d778c3f6333e17dd652c40ed89
Malga90 <savemeebarry@gmail.com>
2018-11-20 20:49:01 +0100

    scroll spy in html

--- dist/js/script.min.js
+++ dist/js/script.min.js
@@ -19,22 +19,11 @@ function stickyNavigation() {
 window.addEventListener('scroll', stickyNavigation);
 
 
+
 // Scroll Spy
 
-const handleView = item => {
-    const linkEl = document.querySelector(`#link-${item}`);
-      
-    let offsetHeight = 0.6*(window.innerHeight)
-    inView.offset({
-      bottom:offsetHeight
-    });
-    
-    inView(`#${item}`)
-      .on("enter", () => linkEl.classList.add('is-active'))
-      .on("exit", el  => linkEl.classList.remove('is-active'))
-  };
-  
-  // Apply method on each DOM element 
-  ["one", "two", "three", "four", "five", "six"].forEach(handleView);
-
+function smoothScroll(target, duration) {
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect();
+}
 
