4b9abb0338e63be835caf394aab5448904787ee8
Malga90 <savemeebarry@gmail.com>
2018-12-02 21:46:50 +0100

    form validator in js

--- src/js/script.js
+++ src/js/script.js
@@ -171,19 +171,142 @@ const slide = new Slider('#slider', {
     nextText : "Następny"
 });
 
+
+
 // Form validation 
 
-const ValidateForm = function(form, options) {
-    
+const FormValidate = function(form, options) {
     const defaultOptions = {
         classError : 'error'
     }
 
     this.form = form;
+    this.options = Object.assign({}, defaultOptions, options);
 
-    this.options = Object.assing({}, defaultOptions, options);
-
-
+    
     this.form.setAttribute('novalidate', 'novalidate');
+
+    this.prepareElements();
+    this.bindSubmit();
 };
 
+FormValidate.prototype.prepareElements = function() {
+    const elements = this.form.querySelectorAll('[required]');
+
+    [].forEach.call(elements, function(element) {
+        
+        if (element.nodeName.toUpperCase() === 'INPUT') {
+            const type = element.type.toUpperCase();
+
+            
+            if (type === 'TEXT') {
+                element.addEventListener('input', function(e) {
+                    this.testInputText(e.target);
+                }.bind(this));
+            }
+            if (type === 'EMAIL') {
+                element.addEventListener('input', function(e) {
+                    this.testInputEmail(e.target);
+                }.bind(this));
+            }
+
+        }
+        if (element.nodeName.toUpperCase() === 'TEXTAREA') {
+            element.addEventListener('input', function(e) {
+                this.testInputText(e.target);
+            }.bind(this));
+        }
+        
+    }, this);
+};
+
+FormValidate.prototype.showFieldValidation = function(input, inputIsValid) {
+    if (!inputIsValid) {
+        input.parentElement.classList.add(this.options.classError);
+    } else {
+        input.parentElement.classList.remove(this.options.classError);
+    }
+};
+
+FormValidate.prototype.testInputText = function(input) {
+    let inputIsValid = true;
+    const pattern = input.getAttribute('pattern');
+
+    if (pattern !== null) {
+        const reg = new RegExp(pattern, 'gi');
+        if (!reg.test(input.value)) {
+            inputIsValid = false;
+        }
+    } else {
+        if (input.value === '') {
+            inputIsValid = false;
+        }
+    }
+
+    if (inputIsValid) {
+        this.showFieldValidation(input, true);
+        return true;
+    } else {
+        this.showFieldValidation(input, false);
+        return false;
+    }
+};
+
+FormValidate.prototype.testInputEmail = function(input) {
+    const mailReg = new RegExp('^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$', 'gi');
+
+    if (!mailReg.test(input.value)) {
+        this.showFieldValidation(input, false);
+        return false;
+    } else {
+        this.showFieldValidation(input, true);
+        return true;
+    }
+};
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const cfg = {};
+    const form = new FormValidate(document.querySelector('.form'), cfg);
+});
+
+FormValidate.prototype.bindSubmit = function() {
+    this.form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        let formIsValidated = true;
+        const elements = this.form.querySelectorAll('[required]');
+
+        [].forEach.call(elements, function(element) {
+            if (element.nodeName.toUpperCase() === 'INPUT') {
+                const type = element.type.toUpperCase();
+
+                if (type === 'EMAIL') {
+                    if (!this.testInputEmail(element)) {
+                        formIsValidated = false;
+                    }
+                }
+            
+                if (type === 'TEXT') {
+                    if (!this.testInputText(element)) {
+                        formIsValidated = false;
+                    }
+                }
+        
+            }
+
+            if (element.nodeName.toUpperCase() === 'TEXTAREA') {
+                if (!this.testInputText(element)) {
+                    formIsValidated = false;
+                }
+            }
+            
+        }, this);
+
+        if (formIsValidated) {
+            e.target.submit();
+        } else {
+            return false;
+        }
+    }.bind(this));
+};
